a312ea71abfe7fa31fa1dc60679bbd8649826e85
Oleg Komaristy <darthroid@gmail.com>
2021-05-20 09:10:42 +0300

    Initial Commit

---
+++ BatteryViewer/BatteryViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryViewerApp.swift
+//  BatteryViewer
+//
+//  Created by Oleg Komaristy on 20.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct BatteryViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
